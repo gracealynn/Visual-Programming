0446e539d876de314212bf4e174859ca432b4c2b
gracealynn <145679031+gracealynn@users.noreply.github.com>
2023-11-23 19:42:15 +0800

    Add files via upload

---
+++ Homepage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Daftar_fasilitas
+{
+    public partial class Homepage : Form
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnmahasiswa_Click(object sender, EventArgs e)
+        {
+            frmdata Frmdata = new frmdata();
+            Frmdata.Show();
+            this.Hide();
+        }
+
+        private void btnfasilitas_Click(object sender, EventArgs e)
+        {
+            Fasilitas fasilitas = new Fasilitas();
+            fasilitas.Show();
+            this.Hide();
+        }
+    }
+}
